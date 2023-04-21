0258a0ea82c85d555521a4eed7248281e70b4096
Lindsey-Harris <h.lindsey16@yahoo.com>
2023-04-21 19:23:51 -0400

    second commit (#6)

--- lowes-capstone/src/Pages/Register.jsx
+++ lowes-capstone/src/Pages/Register.jsx
@@ -1,83 +1,104 @@
-import React, { useState } from 'react';
-import zxcvbn from 'zxcvbn'; // library to check the strength of the password & provide feedback to the user
+import React from "react";
+import zxcvbn from "zxcvbn"; //library used to test the strength of the password the client creates
 
-export const Register = (props) => {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
-  const [name, setName] = useState('');
-  const [score, setScore] = useState(0);
+// used OOP(Object Oriented Programming)
+//create a class that extends the React.Component class.
 
-  const handleSubmit = (e) => {
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      pass: "",
+      name: "",
+      score: 0,
+    };
+    //move the state variables and functions into the Register class and use the `this` keyword 
+    //to reference 
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+//class also includes a constructor method that initializes the state and binds the 
+//handleSubmit and handlePasswordChange methods to `this`.
+  
+  handleSubmit(e) {
     e.preventDefault();
-    console.log(email);
+    console.log(this.state.email);
   }
 
-  const handlePasswordChange = (e) => {
+  handlePasswordChange(e) {
     const password = e.target.value;
     const { score } = zxcvbn(password);
-    setPass(password);
-    setScore(score);
-  };
-
-  let scoreMessage;
-  switch (score) {
-    case 0:
-      scoreMessage = '';
-      break;
-    case 1:
-    case 2:
-      scoreMessage = 'Very weak';
-      break;
-    case 3:
-      scoreMessage = 'Fair';
-      break;
-    case 4:
-      scoreMessage = 'Strong';
-      break;
-    case 5:
-      scoreMessage = 'Very strong';
-      break;
-    default:
-      scoreMessage = '';
-      break;
+    this.setState({ pass: password, score: score });
   }
 
-  return (
-    <div className="auth-form-container">
-      <h2>Register</h2>
-      <form className="register-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">Full Name</label>
-        <input
-          value={name}
-          name="name"
-          onChange={(e) => setName(e.target.value)}
-          id="name"
-          placeholder="full Name"
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="youremail@gmail.com"
-          id="email"
-          name="email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          value={pass}
-          onChange={handlePasswordChange}
-          type="password"
-          placeholder="********"
-          id="password"
-          name="password"
-        />
-        <div>Password strength: {scoreMessage}</div>
-        <button type="submit">Sign Up</button>
-      </form>
-      <button className="link-btn" onClick={() => props.onFormSwitch('login')}>
-        Already have an account? Login here.
-      </button>
-    </div>
-  );
-};
+  render() {
+    let scoreMessage;
+    switch (this.state.score) {
+      case 0:
+        scoreMessage = "";
+        break;
+      case 1:
+      case 2:
+        scoreMessage = "Very weak";
+        break;
+      case 3:
+        scoreMessage = "Fair";
+        break;
+      case 4:
+        scoreMessage = "Strong";
+        break;
+      case 5:
+        scoreMessage = "Very strong";
+        break;
+      default:
+        scoreMessage = "";
+        break;
+    }
+
+    return (
+      <div className="auth-form-container">
+        <h2>Register</h2>
+        <form className="register-form" onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Full Name</label>
+          <input
+            value={this.state.name}
+            name="name"
+            onChange={(e) => this.setState({ name: e.target.value })}
+            id="name"
+            placeholder="full Name"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })}
+            type="email"
+            placeholder="youremail@gmail.com"
+            id="email"
+            name="email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            value={this.state.pass}
+            onChange={this.handlePasswordChange}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+          />
+          <div>Password strength: {scoreMessage}</div>
+          <button type="submit">Sign Up</button>
+        </form>
+        <button
+          className="link-btn"
+          onClick={() => this.props.onFormSwitch("login")}
+        >
+          Already have an account? Login here.
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Register;
