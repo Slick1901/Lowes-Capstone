ef71cd28b3a8021ee2d468ac8eb2079ce6a849c6
Lindsey-Harris <h.lindsey16@yahoo.com>
2023-04-21 23:22:07 -0400

    commit

--- lowes-capstone/src/App.js
+++ lowes-capstone/src/App.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./Login.css";
 import { Login } from "./Pages/Login";
 import  Register  from "./Pages/Register"; //OOP
-// import Footer from "./Components/footer";
-// import Home from "./Pages/Home";
+import Footer from "./Components/footer";
+import Home from "./Pages/Home";
 
 function App() {
   const [currentForm, setCurrentForm] = useState("login");
@@ -13,14 +13,15 @@ function App() {
   };
 
   return (
+    
     <div className="App">
       {currentForm === "login" ? (
         <Login onFormSwitch={toggleForm} />
       ) : (
         <Register onFormSwitch={toggleForm} />
       )}
-      {/* <Home />
-      <Footer /> */}
+      <Home />
+      <Footer />
     </div>
   );
 }
