8ffd37fb8fbb9da38dcbb39394ddbea26e69f9d4
Bryan Olvera <119621661+BryanOlvera1@users.noreply.github.com>
2023-04-27 09:48:36 -0400

    Backend (#14)
    * added founders component which includes cards for each group member
    
    * changed multiple page such as the register page, nav and made a new eventssignuppage

---
+++ lowes-capstone/src/Pages/EventSignUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Map from '../Components/Map';
+import Nav from '../Components/Nav';
+import Footer from '../Components/footer';
+// import { ChakraProvider, theme } from '@chakra-ui/react'
+function EventSignUpPage() {
+  return (
+    <div>
+<Nav/>
+<Map/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default EventSignUpPage;
