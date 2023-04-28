b8c6a0880d29b513c7fb3efe91cfdad9209a819d
Bryan Olvera <119621661+BryanOlvera1@users.noreply.github.com>
2023-04-28 13:24:56 -0400

    Backend (#22)
    * added founders component which includes cards for each group member
    
    * changed multiple page such as the register page, nav and made a new eventssignuppage
    
    * made banner component and added form for event sign up
    
    * event form connecting to db, styling changes are needed
    
    * made new route for database for a newsletter and made the newsletter functional, had to change footer styling
    
    * chnaged styling to newsletter,  and eventform . and made fixes to about us page.

--- lowes-capstone/src/Components/Founders.js
+++ lowes-capstone/src/Components/Founders.js
@@ -21,7 +21,7 @@ return (
 
 <MDBRow className='row-cols-3 row-cols-md-2 g-4'>
 <MDBCol className='hover-shadow'>
-  <MDBCard className='h-100'>
+  <MDBCard className='h-100' style={{ backgroundColor: '#6CB4EE', color: 'white' }} >
     <MDBCardImage className='pics'
       src='./images/bryan.jpeg'
       alt='...'
@@ -30,7 +30,9 @@ return (
     <MDBCardBody>
       <MDBCardTitle className="member-name">Bryan Olvera</MDBCardTitle>
       <MDBCardText className="member-bio">
+      <p style={{ fontSize: '1.3rem' }}>
       Bryan contributed as a full-stack developer role in the development of Home Improvement. With a good foundation in both front-end and back-end development, Bryan made great contributions to the project's user interface, and overall functionality.
+      </p>
       </MDBCardText>
       <MDBBtn outline color="secondary" floating className='linkedin-icon' href='https://www.linkedin.com/in/bolvera/' target="_blank" role='button'>
       <MDBIcon fab icon='linkedin-in' />
@@ -39,7 +41,7 @@ return (
   </MDBCard>
 </MDBCol>
 <MDBCol className='hover-shadow'>
-  <MDBCard className='h-100'>
+  <MDBCard className='h-100' style={{ backgroundColor: '#6CB4EE', color: 'white' }}>
     <MDBCardImage className='pics'
       src='./images/deb.png'
       alt='...'
@@ -48,7 +50,9 @@ return (
     <MDBCardBody>
       <MDBCardTitle className="member-name">Deborah Case</MDBCardTitle>
       <MDBCardText className="member-bio">
+      <p style={{ fontSize: '1.3rem' }}>
       Deborah is a front-end developer who contributed to Home Improvement. With skills learned throughout the apprenticeship, Deborah played a key role in building the front-end of the project. 
+      </p>
       </MDBCardText>
       <MDBBtn outline color="secondary" floating className='linkedin-icon' href='https://www.linkedin.com/in/deborah-case/' target="_blank" role='button'>
       <MDBIcon fab icon='linkedin-in' />
@@ -57,7 +61,7 @@ return (
   </MDBCard>
 </MDBCol>
 <MDBCol className='hover-shadow'>
-  <MDBCard className='h-100'>
+  <MDBCard className='h-100' style={{ backgroundColor: '#6CB4EE', color: 'white' }}>
     <MDBCardImage className='pics'
       src='./images/lindsey.jpg'
       alt='...'
@@ -66,7 +70,9 @@ return (
     <MDBCardBody>
       <MDBCardTitle className="member-name">Lindsey Harris</MDBCardTitle>
       <MDBCardText className="member-bio">
+      <p style={{ fontSize: '1.3rem' }}>
       Lindsey is a front-end developer who contributed to Home Improvement. With skills acquired through the apprenticeship, Lindsey played a pivotal role in developing the front-end of the project aswell as assisting with the backend.
+      </p>
       </MDBCardText>
       <MDBBtn outline color="secondary" floating className='linkedin-icon' href='https://www.linkedin.com/in/lindsey-harris1228/' target="_blank" role='button'>
       <MDBIcon fab icon='linkedin-in' />
@@ -75,7 +81,7 @@ return (
   </MDBCard>
 </MDBCol>
 <MDBCol className='hover-shadow'>
-  <MDBCard className='h-100'>
+  <MDBCard className='h-100' style={{ backgroundColor: '#6CB4EE', color: 'white' }}>
     <MDBCardImage className='pics'
       src='./images/chanty.jpg'
       alt='...'
@@ -84,7 +90,9 @@ return (
     <MDBCardBody>
       <MDBCardTitle className="member-name">Chanthai Phakdy</MDBCardTitle>
       <MDBCardText className="member-bio">
+      <p style={{ fontSize: '1.3rem' }}>
       Chanthai is a backend developer who contributed to Home Improvement. Using their backend knowledge, Chanthai played a key role in building the backend of the project.
+      </p>
       </MDBCardText>
       <MDBBtn outline color="secondary" floating className='linkedin-icon' href='https://www.linkedin.com/in/chanthaiphakdy/' target="_blank" role='button'>
       <MDBIcon fab icon='linkedin-in' />
@@ -93,7 +101,7 @@ return (
   </MDBCard>
 </MDBCol>
 <MDBCol className='hover-shadow'>
-  <MDBCard className='h-100'>
+  <MDBCard className='h-100' style={{ backgroundColor: '#6CB4EE', color: 'white' }}>
     <MDBCardImage className='pics'
       src='./images/sincere.jpg'
       alt='...'
@@ -102,7 +110,9 @@ return (
     <MDBCardBody>
       <MDBCardTitle className="member-name">Sincere Legere</MDBCardTitle>
       <MDBCardText className="member-bio">
+      <p style={{ fontSize: '1.3rem' }}>
       Sincere is a front-end developer who contributed to Home Improvement. With skills acquired through the apprenticeship, Sincere played a crucial role in developing the front-end of the project.
+      </p>
       </MDBCardText>
       <MDBBtn outline color="secondary" floating className='linkedin-icon' href='https://www.linkedin.com/in/sincere-legere-007141247/' target="_blank" role='button'>
       <MDBIcon fab icon='linkedin-in' />
@@ -111,7 +121,7 @@ return (
   </MDBCard>
 </MDBCol>
 <MDBCol className='hover-shadow'>
-  <MDBCard className='h-100'>
+  <MDBCard className='h-100' style={{ backgroundColor: '#6CB4EE', color: 'white' }}>
   <MDBContainer>
       <section>
         <MDBTypography
@@ -129,7 +139,7 @@ return (
             <MDBTypography tag="h6" className="mb-3 text-primary">
               <MDBIcon far icon="paper-plane text-primary pe-2" /> Do you offer any accommodations for disabled participants?
             </MDBTypography>
-            <p>
+            <p style={{ fontSize: '1.3rem' }}>
              
               We strive to make our workshops accessible to all participants and do make neccesary accommodations. 
             </p>
@@ -138,7 +148,7 @@ return (
             <MDBTypography tag="h6" className="mb-3 text-primary">
               <MDBIcon fas icon="pen-alt text-primary pe-2" /> Can I bring my children to the workshops?
             </MDBTypography>
-            <p>
+            <p style={{ fontSize: '1.3rem' }}>
               Children are welcome to attend our workshops with a parent or guardian. We also offer Workshops for them.
             </p>
           </MDBCol>
@@ -146,7 +156,7 @@ return (
             <MDBTypography tag="h6" className="mb-3 text-primary">
               <MDBIcon fas icon="user text-primary pe-2" />  Where are your workshops held?
             </MDBTypography>
-            <p>
+            <p style={{ fontSize: '1.3rem' }}>
             Our workshops are held at various locations depending on the workshop topic and availability of the venue.
             </p>
           </MDBCol>
@@ -154,7 +164,7 @@ return (
             <MDBTypography tag="h6" className="mb-3 text-primary">
               <MDBIcon fas icon="rocket text-primary pe-2" />  Are your workshops free?
             </MDBTypography>
-            <p>
+            <p style={{ fontSize: '1.4rem' }}>
             Our workshops are free, but we appreciate donations to help us continue offering our services to the community.
             </p>
           </MDBCol>
@@ -162,7 +172,7 @@ return (
             <MDBTypography tag="h6" className="mb-3 text-primary">
               <MDBIcon fas icon="home text-primary pe-2" /> What is the purpose of your organization?
             </MDBTypography>
-            <p>
+            <p style={{ fontSize: '1.3rem' }}>
              
               Our organization aims to help disabled people, children, and women with their home improvement needs by offering in-person workshops and training sessions to enhance their knowledge and skills.
             </p>
@@ -171,7 +181,7 @@ return (
             <MDBTypography tag="h6" className="mb-3 text-primary">
               <MDBIcon fas icon="book-open text-primary pe-2" />  What kind of workshops do you offer?
             </MDBTypography>
-            <p>
+            <p style={{ fontSize: '1.3rem' }}>
             We offer a variety of workshops on topics such as basic home repairs, plumbing, electrical work, carpentry, and gardening. Our workshops are designed to be accessible to everyone, regardless of their skill level.
             </p>
           </MDBCol>
