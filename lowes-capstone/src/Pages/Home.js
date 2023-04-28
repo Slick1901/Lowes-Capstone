1bc4421f40431081deaa4dfb0029bf44c602b643
dcase312 <119532744+dcase312@users.noreply.github.com>
2023-04-28 10:01:22 -0400

    Home page2 (#20)
    * added a package
    
    * Added a hero to the home page as well as created a different display for the workshops to eliminate redundant code. Changed the layout for virtual workshops
    
    * Renamed some of my components
    
    * fixed css issue that I was having with the bootstrap library by adding a class on the outside element of the component to target the bootstrap class specifically in my code so it wouldn't clash with other bootstrap code
    
    * added nav
    
    * pushing to branch
    
    * imported font awesome for card navigation buttons as well as linking card buttons to corresponding page. Created dashboard home page that links to event sign up page
    
    * linked button in hero to about page
    
    * deleted unused code
    
    * pushing new main to branch
    
    ---------
    
    Co-authored-by: Deborah Case <deborahcase@Administrators-MacBook-Pro.local>

--- lowes-capstone/src/Pages/Home.js
+++ lowes-capstone/src/Pages/Home.js
@@ -11,41 +11,25 @@ import Nav from '../Components/Nav';
 
 function Home(item) {
   return (
-    <div >
+    <div className='homeContainer'>
       <Nav />
         <Hero />
 <WorkshopInfo />
         <div className={classes.workshopContainer}>
         <section className={classes.section}>
-        <h2>In-Person Workshops</h2>
+        <h2 className='workshopCategory'>In-Person Workshops</h2>
         <h3>{item.category}</h3>
         <div>
-            {/* <Card props = {data} width='100%'/> */}
             <WorkshopCard props={data}/>
         </div>
         </section>
-        {/* <section className='section'>
-        <h3>Handi-capable Workshops</h3>
-        <div class='cardsWrapper'>
-            <Manny props={data} workshopDetail="Is"/>
-
-        </div>
-        </section>
-        <section className='section'>
-        <h3>Women Empowerment Workshops</h3>
-        <div class='cardsWrapper'>
-            <Manny props={data} workshopDetail="Deborah"/>
-        </div>
-        </section> */}
-        {/* <h3>Virtual Workshops</h3> */}
         <section className={classes.section}>
-       <h3>Virtual Workshops</h3>
+       <h2 className='workshopCategory'>Virtual Workshops</h2>
          <div className={classes.cardsWrapper}>
             <Card props = {data} width='100%'/>
         </div>
         </section>
             </div>
-
             <Footer />
     </div>
   )
