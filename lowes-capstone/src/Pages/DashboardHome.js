7695fae6d59977f3239e7cd2d4496b93df331f95
Deborah Case <deborahcase@Administrators-MacBook-Pro.local>
2023-04-28 00:41:04 -0400

    imported font awesome for card navigation buttons as well as linking card buttons to corresponding page. Created dashboard home page that links to event sign up page

--- lowes-capstone/src/Pages/DashboardHome.js
+++ lowes-capstone/src/Pages/DashboardHome.js
@@ -5,11 +5,11 @@ import data from '../Components/cardData.json'
 import classes from './Home.module.css'
 import Footer from '../Components/footer';
 import Hero from '../Components/Hero';
-import WorkshopCard from '../Components/WorkshopCard';
+import DashboardWorkshopCard from '../Components/DashboardWorkshopCard';
 import WorkshopInfo from '../Components/WorkshopInfo';
 import Nav from '../Components/Nav';
 
-function Home(item) {
+function DashboardHome(item) {
   return (
     <div className='homeContainer'>
       <Nav />
@@ -21,7 +21,7 @@ function Home(item) {
         <h3>{item.category}</h3>
         <div>
             {/* <Card props = {data} width='100%'/> */}
-            <WorkshopCard props={data}/>
+            <DashboardWorkshopCard props={data}/>
         </div>
         </section>
         {/* <section className='section'>
@@ -51,48 +51,4 @@ function Home(item) {
   )
 }
 
-
-
-
-// function Home(){
-//   return (
-//     <div>
-//       <Hero />
-//       <WorkshopInfo />
-//       <div className='hey'>
-//       <h2>In-Person Workshops</h2>
-//       <section className='section'>
-//         <h3>The Basics</h3>
-//         <div class='cardsWrapper'>
-//             <Card props = {data} width='100%'/>
-//         </div>
-//         </section>
-//         <section className='section'>
-        
-//         <h3>Kid Friendly Workshops</h3>
-//         <div class='cardsWrapper'>
-//             <Card props = {data} width='100%'/>
-//         </div>
-//         </section>
-//         <section className='section'>
-//         <h3>Handi-capable Workshops</h3>
-//         <div class='cardsWrapper'>
-//             <Card props = {data} width='100%'/>
-//         </div>
-//         </section>
-//         <section className='section'>
-//         <h3>Women Empowerment Workshops</h3>
-//         <div class='cardsWrapper'>
-//             <Card props = {data} width='100%'/>
-//         </div>
-//         </section>
-//         <h3>Virtual Workshops</h3>
-//             <VirtualCard />
-//             </div>
-
-//             <Footer />
-//     </div>
-//   )
-// }
-
-export default Home
+export default DashboardHome
