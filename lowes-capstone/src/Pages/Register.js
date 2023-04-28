797468f96ef8690c2b5c46265623e12270574fb3
Lindsey-Harris <h.lindsey16@yahoo.com>
2023-04-28 08:57:45 -0400

    Lin (#19)
    * created a login and register component
    
    * updated Logreg component; Added css to Login.css
    
    * changed color a color from white to light grey in the login.css file
    
    * added a picture to images folder. Resized the login page
    
    * redid the login page
    
    * adjusted the css in the login.css file
    
    * designed the front end of the Registration.js file and rerouted the register in the Nav.js file

--- lowes-capstone/src/Pages/Register.js
+++ lowes-capstone/src/Pages/Register.js
@@ -6,55 +6,51 @@ import { Axios } from "axios";
 //create a class that extends the React.Component class.
 
 function Register() {
- 
-      
-const [emailReg, setEmailReg] = useState("");
-const [passwordReg, setPasswordReg] = useState("");
-const [fullnameReg, setfullnameReg] = useState("");
+  const [emailReg, setEmailReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [fullnameReg, setfullnameReg] = useState("");
 
+  const register = () => {
+    Axios.post("http://localhost:3001/register", {
+      email: emailReg,
+      password: passwordReg,
+      fullname: fullnameReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
 
-const register = () =>{
-  Axios.post('http://localhost:3001/register', {
-    email: emailReg,
-    password: passwordReg,
-    fullname: fullnameReg,
-  }).then((response)=>{
-    console.log(response)
-  })
-}
-
-    return (
-      <div className="auth-form-container">
-        <h2>Register</h2>
-        <form className="register-form">
-          <label htmlFor="name">Full Name</label>
-          <input type="text" onChange={(e)=>{
+  return (
+    <div className="auth-form-container">
+      <h2>Register</h2>
+      <form className="register-form">
+        <label htmlFor="name">Full Name</label>
+        <input
+          type="text"
+          onChange={(e) => {
             setfullnameReg(e.target.value);
           }}
-            placeholder="full Name"
-          />
-          <label htmlFor="email">Email</label>
-          <input  type="text" onChange={(e)=>{
+          placeholder="full Name"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          onChange={(e) => {
             setEmailReg(e.target.value);
           }}
-          />
-          <label htmlFor="password">Password</label>
-          <input  type="password" onChange={(e)=>{
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          onChange={(e) => {
             setPasswordReg(e.target.value);
           }}
-          />
-  
-  <button onClick={register}>Register</button>
-        </form>
-        {/* <button
-          className="link-btn"
-          onClick={() => this.props.onFormSwitch("login")}
-        >
-          Already have an account? Login here.
-        </button> */}
-      </div>
-    );
-  }
+        />
 
+        <button onClick={register}>Register</button>
+      </form>
+    </div>
+  );
+}
 
 export default Register;
