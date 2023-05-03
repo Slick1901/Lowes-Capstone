fac74c9d424c0408501d320ece2b3855a48354db
Lindsey-Harris <h.lindsey16@yahoo.com>
2023-05-03 12:05:50 -0400

    Log (#42)
    * added the redirect function for the Login button on the Login page
    
    * added color to my labels on the login and register pages and added email and password validation to the register page
    
    * adjusted the CSS on the contact page
    
    * edited the eventform.css file for the contact page
    
    * commit
    
    * merged with main
    
    * made changes to banner.js and dashboardnav

--- lowes-capstone/src/Components/DashboardNav.js
+++ lowes-capstone/src/Components/DashboardNav.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import logo1 from './logo.jpeg';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo1 from "./logo.jpeg";
 export default class DashboardNav extends Component {
   render() {
     return (
@@ -12,21 +11,25 @@ export default class DashboardNav extends Component {
             <LogoImage src={logo1} alt="home" />
           </Link>
         </LogoWrapper>
-
         <MenuItems>
           <MenuItem>
             <Link to="/DashboardAbout" className="nav-link">
-            About Us
+              About Us
             </Link>
           </MenuItem>
           <MenuItem>
             <Link to="/EventCart" className="nav-link">
-            Events
+              Events
             </Link>
           </MenuItem>
           <MenuItem>
             <Link to="/DashboardContact" className="nav-link">
-            Contact
+              Contact
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link to="/Login" className="nav-link">
+              Logout
             </Link>
           </MenuItem>
         </MenuItems>
@@ -34,93 +37,35 @@ export default class DashboardNav extends Component {
     );
   }
 }
-
 const NavWrapper = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: #222831;
   padding: 1rem;
-  fontSize: 1.5rem;
+  fontsize: 1.5rem;
   @media (min-width: 576px) {
     padding: 1rem 2rem;
   }
 `;
-
 const LogoWrapper = styled.div`
   display: flex;
   align-items: center;
 `;
-
 const LogoImage = styled.img`
   height: 40px;
   width: auto;
-
   @media (min-width: 576px) {
     height: 50px;
   }
 `;
-
 const MenuItems = styled.ul`
   display: flex;
-  fontSize: 1.5rem;
+  fontsize: 1.5rem;
   justify-content: center;
   align-items: center;
 `;
-
 const MenuItem = styled.li`
   margin: 0 1rem;
-  font-size: 1.5rem;
+  font-size: 1rem;
 `;
-
-
-
-
-// import React, { Component } from 'react'
-// import {Link} from 'react-router-dom';
-// import logo from './homelogo.png';
-// import styled from 'styled-components';
-
-// export default class DashboardNav extends Component {
-//     render() {
-//         return (
-//             <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
-//                <ul className="navbar-nav align-items-center">
-//                <Link to='/dashboardHome'>
-//                   <img src={logo} alt="home" className="navbar-brand" />
-//                </Link> 
-                
-//                </ul>
-//                <ul className="navbar-nav align-items-center">
-//                  <li className="nav-item ml-5">
-//                     <Link to="/about" className="nav-link">
-//                        About Us
-//                     </Link>
-//                  </li>
-//                </ul>
-//                <ul className="navbar-nav align-items-center">
-//                  <li className="nav-item ml-5">
-//                     <Link to="/events" className="nav-link">
-//                        Events
-//                     </Link>
-//                  </li>
-//                </ul>
-//             </NavWrapper>
-//         )
-//     }
-// }
-
-// const NavWrapper = styled.nav`
-// display:flex;
-// justify-content:space-between;
-// background-color:   #6CB4EE;
-// .nav-link{
-//     color:white;
-//     font-size: 1.2rem; 
-//     text-transform:capitalize;
-// }
-// .navbar-brand{
-//   height:100px;
-//   width:250px;
-// }
-// `;
--- lowes-capstone/src/Components/Nav.js
+++ lowes-capstone/src/Components/Nav.js
@@ -1,83 +1,86 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import logo1 from './logo.jpeg';
-
-export default class Nav extends Component {
-  render() {
-    return (
-      <NavWrapper>
-        <LogoWrapper>
-          <Link to="/">
-            <LogoImage src={logo1} alt="home" />
-          </Link>
-        </LogoWrapper>
-
-        <MenuItems>
-          <MenuItem>
-            <Link to="/about" className="nav-link">
-              About Us
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/register" className="nav-link">
-              Register
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/Contact" className="nav-link">
-             Contact
-            </Link>
-          </MenuItem>
-        </MenuItems>
-      </NavWrapper>
-    );
-  }
-}
 const NavWrapper = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #222831;
-  padding: 1rem;
-  font-size: 1.3rem;
-
-  @media (min-width: 576px) {
-    padding: 1rem 2rem;
-  }
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ background-color: #222831;
+ padding: 1rem;
+ font-size: 1rem;
 `;
-
 const LogoWrapper = styled.div`
-  display: flex;
-  align-items: center;
+ display: flex;
+ align-items: center;
 `;
-
 const LogoImage = styled.img`
-  height: 40px;
-  width: auto;
-
-  @media (min-width: 576px) {
-    height: 50px;
-  }
+ height: 40px;
+ width: auto;
 `;
 
 const MenuItems = styled.ul`
-  display: row;
-  font-size: 1.3rem;
-
-  @media (min-width: 576px) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+ display: flex;
+ flex-direction: row;
+ list-style-type: none;
+ position: static;
+ background-color: transparent;
+ margin: 0;
+ width: auto;
+ padding: 0;
+ margin-left: auto;
 `;
-
 const MenuItem = styled.li`
+ margin: 0.5rem 0;
+ @media (min-width: 576px) {
   margin: 0 1rem;
-  font-size: 1.3rem;
+ }
 `;
+const Nav = () => {
+
+ return (
+  <NavWrapper>
+   <LogoWrapper>
+    <Link to="/">
+     <LogoImage src={logo1} alt="home" />
+    </Link>
+   </LogoWrapper>
+   
+   <MenuItems>
+    <MenuItem>
+     <Link to="/about" className="nav-link">
+      About Us
+     </Link>
+    </MenuItem>
+    <MenuItem>
+     <Link to="/login" className="nav-link">
+      Login
+     </Link>
+    </MenuItem>
+    <MenuItem>
+     <Link to="/register" className="nav-link">
+      Register
+     </Link>
+    </MenuItem>
+    <MenuItem>
+     <Link to="/Contact" className="nav-link">
+      Contact
+     </Link>
+    </MenuItem>
+   </MenuItems>
+  </NavWrapper>
+ );
+};
+export default Nav;
+
+
+
+
+
+
+
+
+
+
+
+
