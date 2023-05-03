8e953ecf18fc3ccf4a950cacc8762bd9696e5276
Lindsey-Harris <h.lindsey16@yahoo.com>
2023-05-03 12:01:18 -0400

    made changes to banner.js and dashboardnav

--- lowes-capstone/src/Components/Banner.js
+++ lowes-capstone/src/Components/Banner.js
@@ -1,26 +1,22 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 function Banner({ message }) {
-  const [showBanner, setShowBanner] = useState(true);
-
-  function handleClose() {
-    setShowBanner(false);
-  }
-
-  return (
-    showBanner && (
-      <div style={{ backgroundColor: 'lightgrey', padding: '15px' }}>
-        <span style={{ color:'black', fontSize: "1.2rem" }}>Logged in! You are able to register for an event.</span>
-        <Link to="/EventCart">
-          <button style={{ float: 'right', marginTop: '-25px', marginRight: '10px' }}>Sign Up for an event</button>
-        </Link>
-        <button onClick={handleClose} style={{ float: 'right', marginTop: '-25px'}}>
-          X
-        </button>
-
-      </div>
-    )
-  );
+ const [showBanner, setShowBanner] = useState(true);
+ function handleClose() {
+  setShowBanner(false);
+ }
+ return (
+  showBanner && (
+   <div style={{ backgroundColor: 'lightgrey', padding: '15px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <span style={{ color:'black', fontSize: "1.2rem" }}>Logged in! You are able to register for an event.</span>
+    <div>
+     <Link to="/EventCart">
+      <button style={{ marginRight: '10px' }}>Sign Up for an event</button>
+     </Link>
+     <button onClick={handleClose}>X</button>
+    </div>
+   </div>
+  )
+ );
 }
-
 export default Banner;
